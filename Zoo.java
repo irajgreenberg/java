d4fee8e95348d0dab9286d4d2fdfd8f55de940b2
ira Greenberg <irajgreenberg@gmail.com>
2016-04-05 16:33:36 -0500

    updated Zoo example

--- Zoo.java
+++ Zoo.java
@@ -1,24 +1,37 @@
-public abstract class Mammal {
+abstract class Mammal {
 	public String toString( ){
 		return "Mammal type";
+	}
+
+	abstract void eat();
+
+	// concrete method
+	void breath(){
 	}
 	
 }
 
-public abstract class Insect {
+abstract class Insect {
 	public String toString( ){
 		return "Insect type";
 	}
+
+	abstract void eat();
 }
 
 interface Motile {
-	// float getDistTraveled();
+	// all methods are abstract by default
+	// not allowed to include concrete methods
+
+	 public float getDistTraveled();
 	// void setSpeed();
 	// void setOrigin(String origin);
 	// void setDestination(String destination);
 }
 
 interface Flyable extends Motile {
+	void fly();
+
 	// void fly();
 	// void land();
 }
@@ -29,15 +42,51 @@ interface Predatory {
 }
 
 
-public class Lemur extends Mammal implements Flyable {
-	public String toString( ){
-		return "Lemur type";
+class Dog extends Mammal {
+	void eat(){
+		// feed the dog appropriately
+		System.out.println("I'm eating a bone");
+
 	}
 }
 
-public class Dragonfly extends Insect implements Flyable, Predatory{
+class Lemur extends Mammal implements Flyable {
+	public void fly() {
+		System.out.println("The Lemur is soaring!!");
+	}
+
+	public void eat(){
+		// feed the lemur appropriately
+		System.out.println("I'm eating fruit");
+	}
+
+	public String toString( ){
+		return "Lemur type";
+	}
+
+	public float getDistTraveled() {
+		// create real implementation eventually
+		return 1.0f;
+	}
+}
+
+class Dragonfly extends Insect implements Flyable, Predatory{
+	public void fly() {
+		System.out.println("The Drongfly is hovering!!");
+	}
+
 	public String toString( ){
 		return "Dragonfly type";
+	}
+
+	public float getDistTraveled() {
+		// create real implementation eventually
+		return 1.0f;
+	}
+
+	public void eat(){
+		// feed the dragonfly appropriately
+		System.out.println("I'm eating fruit");
 	}
 }
 
@@ -51,10 +100,21 @@ public class Zoo {
 
  public Zoo(Flyable[] creatures){
  	this.creatures = creatures;
+ 	System.out.println("in Zoo overloaded CSTR");
+ 	for(Flyable f: creatures) {
+ 		f.fly();
+ 	}
  }
 
  public static void main(String[] args){
- 	new Zoo();
+ 	Flyable[] creatures = {
+ 		new Lemur(),
+ 		new Dragonfly()
+ 	};
+ 	
+ 	new Zoo(creatures);
+
+ 	Zoo z = new Zoo();
  }
 	
 }
